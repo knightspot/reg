38a8507acae2445291a9ebcdaceb554fc024b73d
Jonas Petersson <jonasp@telia.com>
2017-02-15 23:02:41 +0100

    Add mockito dependency

--- artifact/src/test/java/group/artifact/SimpleReplaceTest.java
+++ artifact/src/test/java/group/artifact/SimpleReplaceTest.java
@@ -1,11 +1,8 @@
 package group.artifact;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
